368b9ffcba18d0edfcc1bae141ffce13911af342
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-01-07 15:38:19 +0700

    [feat] api delete secgroup

--- vcontainer/services/network/v2/secgroup/requests_utils.go
+++ vcontainer/services/network/v2/secgroup/requests_utils.go
@@ -4,3 +4,10 @@ func NewCreateOpts(pProjectID string, pOpts *CreateOpts) ICreateOptsBuilder {
 	pOpts.ProjectID = pProjectID
 	return pOpts
 }
+
+func NewDeleteOpts(pProjectID, pSecgroupUUID string) IDeleteOptsBuilder {
+	opts := new(DeleteOpts)
+	opts.ProjectID = pProjectID
+	opts.SecgroupUUID = pSecgroupUUID
+	return opts
+}
